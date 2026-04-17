83b526c25d8fc3b52f1bf9c056517f260d6786ed
valen <ghazeevachloyeeva@gmail.com>
2026-04-17 10:41:22 +0700

    loop

--- basicprog/basic_program/src/loop.java
+++ basicprog/basic_program/src/loop.java
@@ -6,9 +6,11 @@ public class loop {
 
         System.out.println("Masukkan jumlah segitiga: ");
         int n = putar.nextInt();
-;
         for ( int i = 1; i <= n; i++) { // iterasi luar
-            for (int j = 1; j <= i; j++) { // iterasi dalam
+            for (int j = 1; j <= n - i; j++) { // iterasi dalam untuk spasi
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i) - 1; k++) { // iterasi dalam untuk bintang
                 System.out.print("*");
             }
             System.out.println();
