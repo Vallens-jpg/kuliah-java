9b5d1f60a06a2244351aee685cc5c96be30611b1
valen <ghazeevachloyeeva@gmail.com>
2026-04-22 15:10:24 +0700

    materi pertemuan 8

---
+++ basicprog/basic_program/src/Praktikum_8.java
@@ -0,0 +1,11 @@
+public class Praktikum_8 {
+        public static void main(String[] args) {
+            Rumus way = new Rumus();
+            int luas = 0;
+            luas = way.luasPersegiPanjang(8, 9);
+
+            System.out.println("Hasil Panjang X Lebar adalah: " + luas);
+
+            way.luas_persegi("Vitalik Butterin");
+        }
+}
---
+++ basicprog/basic_program/src/Rumus.java
@@ -0,0 +1,12 @@
+public class Rumus {
+
+    public void luas_persegi(String nama) {
+        System.out.println("halo void");
+        System.out.println("halo saya adalah " + nama);
+    }
+
+    public int luasPersegiPanjang (int pa, int le) {
+        return pa * le;
+    }
+
+}
